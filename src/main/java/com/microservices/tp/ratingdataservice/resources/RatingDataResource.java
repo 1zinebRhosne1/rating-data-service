c5676fa342442503f337f66de32c59e57216dcf6
1zinebRhosne1 <54914775+1zinebRhosne1@users.noreply.github.com>
2022-01-21 16:16:35 +0100

    Update RatingDataResource.java

--- src/main/java/com/microservices/tp/ratingdataservice/resources/RatingDataResource.java
+++ src/main/java/com/microservices/tp/ratingdataservice/resources/RatingDataResource.java
@@ -6,20 +6,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingDataResource {
-    @RequestMapping("/movies/{movieId}")
+    @RequestMapping("/{movieId}")
     public Rating getMovieRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
     }
 
-    @RequestMapping("/user/{userId}")
+    @RequestMapping("/users/{userId}")
     public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        List<Rating> ratings= Arrays.asList(
+               new Rating("1234",4),
+                new Rating("5678",3)
+        );
         UserRating userRating = new UserRating();
-        userRating.initData(userId);
+        userRating.setUserRating(ratings);
         return userRating;
 
     }
 
 }
+
